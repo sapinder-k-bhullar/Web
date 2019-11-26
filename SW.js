f29a9ffa38d7b7efdf9ece1a3cdb3058f3e5ce02
A00179193 <simmi08979@gmail.com>
2019-11-25 21:30:00 -0500

    this is running

---
+++ SW.js
@@ -0,0 +1,22 @@
+
+    if ('serviceWorker' in navigator) {
+       window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js');
+        });
+    }
+
+    importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+    workbox.precaching.precacheAndRoute([
+        '/localStorage.html',
+        '/js/TODO.js',
+        '/css/TODO.css'
+    ]);
+    
+    workbox.routing.registerRoute(
+        /\.htm(l?)$/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'html-cache',
+        })
+    );
+    
